d570f2ba183284a22f43c0d2c4530525a420a44c
Stephane Larocque <stephl001@gmail.com>
2018-07-12 23:35:20 -0400

    Implement EnigmaMachine mapper

--- test.fsx
+++ test.fsx
@@ -1,5 +1,3 @@
-type RotorId = I|II|III|IV|V|VI|VII|VIII
-type ReflectorId = A|B|C|ETW
 type Letter = A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z
 type Mapping = private Mapping of Letter array
 
@@ -48,6 +46,8 @@ module Letter =
         | 18 -> S | 19 -> T | 20 -> U | 21 -> V | 22 -> W | 23 -> X | 24 -> Y | 25 -> Z
         | _ -> failwith "Index out of range"
 
+    let charToLetter (c:char) = int c - int 'A' |> fromIndex
+
     let (%+) m n = (((m % n) + n) % n)
     let modAlphabet x = x %+ LettersCount
     let fromModIndex = modAlphabet >> fromIndex
@@ -65,6 +65,10 @@ module Mapping =
         if lettersCount <> 26
         then failwith "A mapping must be composed of 26 distinct letters"
         else Mapping letters
+
+    let fromString (strMapping:string) = 
+        strMapping 
+        |> (Seq.map charToLetter >> Array.ofSeq >> create)
           
     let id = Mapping [|A;B;C;D;E;F;G;H;I;J;K;L;M;N;O;P;Q;R;S;T;U;V;W;X;Y;Z|]
 
@@ -88,21 +92,25 @@ module Mapping =
     let mapLetter (Mapping mapping) (IndexLetter letterIndex) =
         mapping.[letterIndex]
 
+    let reverseMapLetter = inverseMapping >> mapLetter
+
     let offsetMapping offset mapper = 
         offsetLetter offset >> mapper >> reverseOffsetLetter offset
 
 module Rotor =
-    let rotorI = {Notch=Q; Mapping=Mapping.create [|E;K;M;F;L;G;D;Q;V;Z;N;T;O;W;Y;H;X;U;S;P;A;I;B;R;C;J|]; InnerRingOffset=A}
-    let rotorII = {Notch=E; Mapping=Mapping.create [|A;J;D;K;S;I;R;U;X;B;L;H;W;T;M;C;Q;G;Z;N;P;Y;F;V;O;E|]; InnerRingOffset=A}
-    let rotorIII = {Notch=V; Mapping=Mapping.create [|B;D;F;H;J;L;C;P;R;T;X;V;Z;N;Y;E;I;W;G;A;K;M;U;S;Q;O|]; InnerRingOffset=A}
-    let rotorIV = {Notch=J; Mapping=Mapping.create [|E;S;O;V;P;Z;J;A;Y;Q;U;I;R;H;X;L;N;F;T;G;K;D;C;M;W;B|]; InnerRingOffset=A}
-    let rotorV = {Notch=Z; Mapping=Mapping.create [|V;Z;B;R;G;I;T;Y;U;P;S;D;N;H;L;X;A;W;M;J;Q;O;F;E;C;K|]; InnerRingOffset=A}
+    let rotorI = {Notch=Q; Mapping=Mapping.fromString "EKMFLGDQVZNTOWYHXUSPAIBRCJ"; InnerRingOffset=A}
+    let rotorII = {Notch=E; Mapping=Mapping.fromString "AJDKSIRUXBLHWTMCQGZNPYFVOE"; InnerRingOffset=A}
+    let rotorIII = {Notch=V; Mapping=Mapping.fromString "BDFHJLCPRTXVZNYEIWGAKMUSQO"; InnerRingOffset=A}
+    let rotorIV = {Notch=J; Mapping=Mapping.fromString "ESOVPZJAYQUIRHXLNFTGKDCMWB"; InnerRingOffset=A}
+    let rotorV = {Notch=Z; Mapping=Mapping.fromString "VZBRGITYUPSDNHLXAWMJQOFECK"; InnerRingOffset=A}
 
 module Reflector = 
-    let reflectorA = {Mapping=Mapping.create [|E;J;M;Z;A;L;Y;X;V;B;W;F;C;R;Q;U;O;N;T;S;P;I;K;H;G;D|]}
-    let reflectorB = {Mapping=Mapping.create [|Y;R;U;H;Q;S;L;D;P;X;N;G;O;K;M;I;E;B;F;Z;C;W;V;J;A;T|]}
-    let reflectorC = {Mapping=Mapping.create [|F;V;P;J;I;A;O;Y;E;D;R;Z;X;W;G;C;T;K;U;Q;S;B;N;M;H;L|]}
-    let reflectorETW = {Mapping=Mapping.id}
+    let (reflectorA:Reflector) = {Mapping=Mapping.fromString "EJMZALYXVBWFCRQUONTSPIKHGD"}
+    let (reflectorB:Reflector) = {Mapping=Mapping.fromString "YRUHQSLDPXNGOKMIEBFZCWVJAT"}
+    let (reflectorC:Reflector) = {Mapping=Mapping.fromString "FVPJIAOYEDRZXWGCTKUQSBNMHL"}
+    let (reflectorETW:Reflector) = {Mapping=Mapping.id}
+
+    let mapLetter (reflector:Reflector) = reflector.Mapping |> Mapping.mapLetter
 
 module Wheel =
     open Letter
@@ -123,21 +131,39 @@ module Wheel =
             IsInNotchPosition = (wheel.Rotor.Notch = newPos)
         }
 
-    let private innerRingMapper wheel mapping = 
+    let private innerRingMapper mapping wheel = 
         mapping
         |> Mapping.shiftMapping wheel.Rotor.InnerRingOffset
         |> Mapping.mapLetter
 
-    let private mapLetterWithMapping wheel mapping = 
+    let private mapLetterWithMapping mapping wheel = 
         let (IndexLetter indLetter) = wheel.RotorPosition
-        mapping
-        |> innerRingMapper wheel
+        wheel
+        |> innerRingMapper mapping
         |> Mapping.offsetMapping indLetter
 
     let mapLetter wheel =
-        mapLetterWithMapping wheel wheel.Rotor.Mapping
+        mapLetterWithMapping wheel.Rotor.Mapping wheel 
 
     let reverseMapLetter wheel =
-        wheel.Rotor.Mapping 
-        |> Mapping.inverseMapping
-        |> mapLetterWithMapping wheel
+        let inverseMapping = wheel.Rotor.Mapping |> Mapping.inverseMapping
+        wheel |> mapLetterWithMapping inverseMapping
+
+module Plugboard =
+    let mapLetter plugboard = plugboard.Mapping |> Mapping.mapLetter
+    let reverseMapLetter plugboard = plugboard.Mapping |> Mapping.reverseMapLetter
+
+module EnigmaMachine =
+    let mapLetter machine =
+        [
+            machine.Plugboard |> Plugboard.mapLetter
+            machine.Wheel3 |> Wheel.mapLetter
+            machine.Wheel2 |> Wheel.mapLetter
+            machine.Wheel1 |> Wheel.mapLetter
+            machine.Reflector |> Reflector.mapLetter
+            machine.Wheel1 |> Wheel.reverseMapLetter
+            machine.Wheel2 |> Wheel.reverseMapLetter
+            machine.Wheel3 |> Wheel.reverseMapLetter
+            machine.Plugboard |> Plugboard.reverseMapLetter
+        ]
+        |> List.reduce (>>)
